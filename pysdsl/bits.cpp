4514fb02d337ccadf1a8c17527fd40822a4b9be1
Konstantin Podshumok <kpp.live@gmail.com>
2018-01-31 19:41:40 +0300

    bind bits static functions to the module itself

--- pysdsl/bits.cpp
+++ pysdsl/bits.cpp
@@ -1,6 +1,6 @@
 /*cppimport
 <%
-cfg['compiler_args'] = ['-std=c++14', '-ftemplate-depth=4096']
+cfg['compiler_args'] = ['-std=c++14', '-ftemplate-depth=3600']
 cfg['include_dirs'] = ['sdsl-lite/include']
 cfg['libraries'] = ['sdsl', 'divsufsort', 'divsufsort64']
 %>
@@ -48,13 +48,19 @@ auto sel = [] (uint64_t x, uint32_t i) {
     }
     return sdsl::bits::sel(x, i);
 };
+auto sel11 = [] (uint64_t x, uint32_t i, uint32_t c=0) {
+    if (i >= sizeof(sdsl::bits::ps_overflow) / sizeof(sdsl::bits::ps_overflow[0])) {
+        throw py::index_error(std::to_string(i));
+    }
+    return sdsl::bits::sel11(x, i, c);
+};
 
 
 PYBIND11_MODULE(bits, m) {
     m.doc() = "bitwise tricks on 64 bit words.";
 
 
-    py::class_<sdsl::bits>(m, "bits")
+    auto bits_cls = py::class_<sdsl::bits>(m, "bits")
         .def_property_readonly_static("all_set", [](py::object /* self */) { return sdsl::bits::all_set; })
         .def_static("all_set_", []() { return sdsl::bits::all_set; },
                     "64bit mask with all bits set to 1.")
@@ -143,32 +149,41 @@ PYBIND11_MODULE(bits, m) {
         .def_static("sel",  sel, py::arg("x"), py::arg("i"),
                     "Calculate the position of the i-th rightmost 1 bit in the 64bit integer x\n"
                     "x: 64bit integer.\ni: Argument i must be in the range [1..cnt(x)].")
+        .def_static("sel11", sel11, py::arg("x"), py::arg("i"), py::arg("c") = 0,
+                     "The position (in 1..63) of the i-th 11-bit-pattern which terminates a Fibonacci coded integer in x if "
+                     "x contains at least i 11-bit-patterns and a undefined value otherwise.\n"
+                     "x: 64 bit integer.\n"
+                     "i: Index of 11-bit-pattern. i in [1..cnt11(x)]\n"
+                     "c: Carry bit from word before")
+        .def_static("hi11", &sdsl::bits::hi11, py::arg("x"),
+                    "The position (in 1..63) of the leftmost 1 of the leftmost 11-bit-pattern which "
+                    "terminates a Fibonacci coded integer in x if x contains a 11-bit-pattern and 0 otherwise.")
+        .def_static("rev", &sdsl::bits::rev, "reverses a given 64 bit word")
         ;
 
-    m.def("cnt", &sdsl::bits::cnt, py::arg("x"), "Counts the number of set bits in x.");
-    m.def("cnt32", &sdsl::bits::cnt, py::arg("x"), "Counts the number of set bits in 32-bit integer x.");
-    m.def("hi", &sdsl::bits::hi, py::arg("x"),
-          "The position (in 0..63) of the most significant set bit in `x` or 0 if x equals 0.");
-    m.def("lo", &sdsl::bits::lo, py::arg("x"),
-          "The position (in 0..63) of the rightmost 1-bit in the 64bit integer x if x>0 and 0 if x equals 0.");
-    m.def("cnt11", (uint32_t (*) (uint64_t)) &sdsl::bits::cnt11, py::arg("x"),
-          "Count the number of consecutive and distinct 11 in the 64bit integer x.\n"
-          "x: 64bit integer to count the terminating sequence 11 of a Fibonacci code.");
-    m.def("cnt11", cnt11, "Count the number of consecutive and distinct 11 in the 64bit integer x.\n"
-                          "x: 64bit integer to count the terminating sequence 11 of a Fibonacci code.\n"
-                          "c: Carry equals msb of the previous 64bit integer.", py::arg("x"), py::arg("c"));
-    m.def("cnt10", cnt10, "Count 10 bit pairs in the word x.\n"
-                          "x: 64bit integer to count the 10 bit pairs.\n"
-                          "c: Carry equals msb of the previous 64bit integer.", py::arg("x"), py::arg("c"));
-    m.def("cnt01", cnt01, "Count 01 bit pairs in the word x.\n"
-                          "x: 64bit integer to count the 01 bit pairs.\n"
-                          "c: Carry equals msb of the previous 64bit integer.", py::arg("x"), py::arg("c"));
+    m.attr("all_set") = bits_cls.attr("all_set_");
+    m.attr("deBruijn64") = bits_cls.attr("deBruijn64_");
+    m.attr("lt_deBruijn_to_idx") = bits_cls.attr("lt_deBruijn_to_idx_");
+    m.attr("lt_fib") = bits_cls.attr("lt_fib_");
+    m.attr("lt_cnt") = bits_cls.attr("lt_cnt_");
+    m.attr("lt_hi") = bits_cls.attr("lt_hi_");
+    m.attr("lo_set") = bits_cls.attr("lo_set_");
+    m.attr("lo_unset") = bits_cls.attr("lo_unset_");
+    m.attr("lt_lo") = bits_cls.attr("lt_lo_");
+    m.attr("lt_sel") = bits_cls.attr("lt_sel_");
+    m.attr("ps_overflow") = bits_cls.attr("ps_overflow_");
 
-    m.def("map10", &sdsl::bits::map10, py::arg("x"), py::arg("c") = 0,
-          "Map all 10 bit pairs to 01 or 1 if c=1 and the lsb=0. All other pairs are mapped to 00.");
-    m.def("map01", &sdsl::bits::map01, py::arg("x"), py::arg("c") = 1,
-          "Map all 01 bit pairs to 01 or 1 if c=1 and the lsb=0. All other pairs are mapped to 00.");
-    m.def("sel",  sel, py::arg("x"), py::arg("i"),
-          "Calculate the position of the i-th rightmost 1 bit in the 64bit integer x\n"
-          "x: 64bit integer.\ni: Argument i must be in the range [1..cnt(x)].");
+    m.attr("cnt") = bits_cls.attr("cnt");
+    m.attr("cnt32") = bits_cls.attr("cnt32");
+    m.attr("hi") = bits_cls.attr("hi");
+    m.attr("lo") = bits_cls.attr("lo");
+    m.attr("cnt11") = bits_cls.attr("cnt11");
+    m.attr("cnt10") = bits_cls.attr("cnt10");
+    m.attr("cnt01") = bits_cls.attr("cnt01");
+    m.attr("map10") = bits_cls.attr("map10");
+    m.attr("map01") = bits_cls.attr("map01");
+    m.attr("sel") = bits_cls.attr("sel");
+    m.attr("sel11") = bits_cls.attr("sel11");
+    m.attr("hi11") = bits_cls.attr("hi11");
+    m.attr("rev") = bits_cls.attr("rev");
 }
