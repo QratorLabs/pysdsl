1bea65b93d6446afb5777323c292160f45975c67
Konstantin Podshumok <kpp.live@gmail.com>
2018-01-31 21:10:37 +0300

    add pybind11 submodule to simplify things for now

--- pysdsl/__init__.cpp
+++ pysdsl/__init__.cpp
@@ -16,21 +16,28 @@ cfg['libraries'] = ['sdsl', 'divsufsort', 'divsufsort64']
 
 namespace py = pybind11;
 
+using sdsl::enc_vector;
 using sdsl::int_vector;
 
-
-template <class T, typename S=uint64_t>
-auto add_class_(py::module& m, const char* name) {
+template <class T, typename S = uint64_t>
+auto add_class_(py::module &m, const char *name)
+{
     return py::class_<T>(m, name)
-        .def_property_readonly("width", (uint8_t (T::*) (void) const) &T::width)
-        .def_property_readonly("data", (const uint64_t* (T::*) (void) const)  &T::data)
+        .def_property_readonly("width", (uint8_t(T::*)(void) const) & T::width)
+        .def_property_readonly("data",
+                               (const uint64_t *(T::*)(void)const) & T::data)
+
         .def("__len__", &T::size, "The number of elements in the int_vector.")
-        .def_property_readonly("size", &T::size, "The number of elements in the int_vector.")
-        .def_property_readonly_static("max_size",  [](py::object /* self */) {
-            return T::max_size();
-        }, "Maximum size of the int_vector.")
-        .def_property_readonly("size_in_mega_bytes", [](const T& self) { return sdsl::size_in_mega_bytes(self); })
+        .def_property_readonly("size", &T::size,
+                               "The number of elements in the int_vector.")
+        .def_property_readonly_static(
+            "max_size", [](py::object /* self */) {
+                return T::max_size();
+            },
+            "Maximum size of the int_vector.")
+        .def_property_readonly("size_in_mega_bytes", [](const T &self) { return sdsl::size_in_mega_bytes(self); })
         .def_property_readonly("bit_size", &T::bit_size, "The number of bits in the int_vector.")
+
         .def("resize", &T::resize, "Resize the int_vector in terms of elements.")
         .def("bit_resize", &T::bit_resize, "Resize the int_vector in terms of bits.")
         .def_property_readonly("capacity", &T::capacity, "Returns the size of the occupied bits of the "
@@ -38,13 +45,15 @@ auto add_class_(py::module& m, const char* name) {
                                                          "greater or equal to the bit_size of the "
                                                          "vector: capacity >= bit_size).")
         .def("__getitem__", [](const T &self, size_t position) -> S {
-            if (position >= self.size()) {
+            if (position >= self.size())
+            {
                 throw py::index_error(std::to_string(position));
             }
             return static_cast<S>(self[position]);
         })
         .def("__setitem__", [](T &self, size_t position, S value) {
-            if (position >= self.size()) {
+            if (position >= self.size())
+            {
                 throw py::index_error(std::to_string(position));
             }
             self[position] = value;
@@ -79,9 +88,14 @@ auto add_class_(py::module& m, const char* name) {
                  sdsl::util::set_random_bits(self, seed);
             },
             py::call_guard<py::gil_scoped_release>(),
-            py::arg_v("seed", 0, "If seed = 0, the time is used to initialize the pseudo random number "
-                                "generator, otherwise the seed parameter is used."),
-           "Sets all bits of the int_vector to pseudo-random bits."
+            py::arg_v(
+                "seed",
+                0,
+                "If seed = 0, the time is used to initialize the pseudo "
+                "random number generator, otherwise the seed parameter is "
+                "used."
+            ),
+            "Sets all bits of the int_vector to pseudo-random bits."
         )
         .def("__imod__", [](T &self, uint64_t m) {
            sdsl::util::mod(self, m);
@@ -97,13 +111,15 @@ auto add_class_(py::module& m, const char* name) {
            return sdsl::util::cnt_zeroone_bits(self);
         }, "Number of occurrences of bit pattern `01` in vector")
         .def("next_bit", [](const T &self, size_t idx) {
-            if (idx >= self.bit_size()) {
+            if (idx >= self.bit_size())
+            {
                 throw py::index_error(std::to_string(idx));
             }
            return sdsl::util::next_bit(self, idx);
         }, py::arg("idx"), "Get the smallest position `i` >= `idx` where a bit is set")
         .def("prev_bit", [](const T &self, size_t idx) {
-            if (idx >= self.bit_size()) {
+            if (idx >= self.bit_size())
+            {
                 throw py::index_error(std::to_string(idx));
             }
            return sdsl::util::prev_bit(self, idx);
@@ -113,7 +129,7 @@ auto add_class_(py::module& m, const char* name) {
 
 
 
-        .def("max", [](const T& self) {
+        .def("max", [](const T &self) {
             return std::max_element(self.begin(), self.end());
         }, py::call_guard<py::gil_scoped_release>())
         .def("min", [](const T& self) {
@@ -125,53 +141,63 @@ auto add_class_(py::module& m, const char* name) {
     ;
 }
 
+auto add_enc_class(py::module &m)
+{
+    py::class_<enc_vector<>>(m, "EncVector")
+        .def(py::init());
+}
 
-PYBIND11_MODULE(pysdsl, m) {
+PYBIND11_MODULE(pysdsl, m)
+{
     m.doc() = "sdsl-lite bindings for python";
+
+    add_enc_class(m);
 
     add_class_<int_vector<0>>(m, "IntVector")
         .def(py::init([](size_t size, uint64_t default_value, uint8_t bit_width) {
-            return int_vector<0>(size, default_value, bit_width);
-        }), py::arg("size"), py::arg("default_value") = 0, py::arg("bit_width") = 64)
+                 return int_vector<0>(size, default_value, bit_width);
+             }),
+             py::arg("size"), py::arg("default_value") = 0, py::arg("bit_width") = 64)
         .def("expand_width", [](int_vector<0> &self, size_t width) {
-           sdsl::util::expand_width(self, width);
-        }, "Expands the integer width to new_width >= v.width().")
+            sdsl::util::expand_width(self, width);
+        },
+             "Expands the integer width to new_width >= v.width().")
         .def("bit_compress", [](int_vector<0> &self) {
-           sdsl::util::bit_compress(self);
-        }, "Bit compress the int_vector. Determine the biggest value X and then set the int_width to the "
-           "smallest possible so that we still can represent X.")
-    ;
+            sdsl::util::bit_compress(self);
+        },
+             "Bit compress the int_vector. Determine the biggest value X and then set the int_width to the "
+             "smallest possible so that we still can represent X.");
 
     add_class_<int_vector<1>, bool>(m, "BitVector")
         .def(py::init([](size_t size, bool default_value) {
-            return int_vector<1>(size, default_value, 1);
-        }), py::arg("size"), py::arg("default_value") = false)
-        .def("flip", &int_vector<1>::flip, "Flip all bits of bit_vector")
-    ;
+                 return int_vector<1>(size, default_value, 1);
+             }),
+             py::arg("size"), py::arg("default_value") = false)
+        .def("flip", &int_vector<1>::flip, "Flip all bits of bit_vector");
 
     add_class_<int_vector<8>, uint8_t>(m, "Int8Vector")
         .def(py::init([](size_t size, uint8_t default_value) {
-            return int_vector<8>(size, default_value, 8);
-        }), py::arg("size"), py::arg("default_value") = 0)
-    ;
+                 return int_vector<8>(size, default_value, 8);
+             }),
+             py::arg("size"), py::arg("default_value") = 0);
 
     add_class_<int_vector<16>, uint16_t>(m, "Int16Vector")
         .def(py::init([](size_t size, uint16_t default_value) {
-            return int_vector<16>(size, default_value, 16);
-        }), py::arg("size"), py::arg("default_value") = 0)
-    ;
+                 return int_vector<16>(size, default_value, 16);
+             }),
+             py::arg("size"), py::arg("default_value") = 0);
 
     add_class_<int_vector<32>, uint32_t>(m, "Int32Vector")
         .def(py::init([](size_t size, uint32_t default_value) {
-            return int_vector<32>(size, default_value, 32);
-        }), py::arg("size"), py::arg("default_value") = 0)
-    ;
+                 return int_vector<32>(size, default_value, 32);
+             }),
+             py::arg("size"), py::arg("default_value") = 0);
 
     add_class_<int_vector<64>, uint64_t>(m, "Int64Vector")
         .def(py::init([](size_t size, uint64_t default_value) {
-            return int_vector<64>(size, default_value, 64);
-        }), py::arg("size"), py::arg("default_value") = 0)
-    ;
+                 return int_vector<64>(size, default_value, 64);
+             }),
+             py::arg("size"), py::arg("default_value") = 0);
 
     //add_class_<int_vector<128>, uint128_t>(m, "Int128Vector")
     //    .def(py::init([](size_t size, uint128_t default_value) {
