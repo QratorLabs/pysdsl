ffc67ee5cc1dfba6036e5d5994916ca43e758e73
Konstantin Podshumok <kpp.live@gmail.com>
2018-02-07 21:11:04 +0300

    overload for_each for non-consts

--- pysdsl/converters.hpp
+++ pysdsl/converters.hpp
@@ -16,8 +16,20 @@ namespace detail
         return std::make_tuple(f(std::get<Is>(t))...);
     }
 
+    template<typename P, typename Function, std::size_t... Is>
+    decltype(auto) for_each_impl(P& t, Function f, std::index_sequence<Is...>)
+    {
+        return std::make_tuple(f(std::get<Is>(t))...);
+    }
+
     template<typename... T, typename Function>
     decltype(auto) for_each(const std::tuple<T...>& t, Function f)
+    {
+        return for_each_impl(t, f, std::index_sequence_for<T...>{});
+    }
+
+    template<typename... T, typename Function>
+    decltype(auto) for_each(std::tuple<T...>& t, Function f)
     {
         return for_each_impl(t, f, std::index_sequence_for<T...>{});
     }
@@ -29,10 +41,9 @@ namespace detail
         add_inits_functor(ToCls &cls_to) : m_cls_to(cls_to) {}
 
         template <typename FromCls>
-        decltype(auto) operator()(FromCls &t)
+        decltype(auto) operator()(FromCls &)
         {
             m_cls_to.def(py::init([](const typename FromCls::type& from) {
-                //py::print("Fast!");
                 return typename ToCls::type(from);
             }), py::arg("v"), py::call_guard<py::gil_scoped_release>());
             return m_cls_to;
@@ -46,6 +57,13 @@ namespace detail
 
 template <typename... Ts, typename F>
 decltype(auto) for_each_in_tuple(const std::tuple<Ts...> &t, F f)
+{
+    return detail::for_each(t, f);
+}
+
+
+template <typename... Ts, typename F>
+decltype(auto) for_each_in_tuple(std::tuple<Ts...> &t, F f)
 {
     return detail::for_each(t, f);
 }
