911dcb163cf6bd6d8500a82e962d1270c6849e8c
Konstantin Podshumok <kpp.live@gmail.com>
2018-02-13 22:51:38 +0300

    add any() and all() methods

--- pysdsl/__init__.cpp
+++ pysdsl/__init__.cpp
@@ -4,11 +4,11 @@ cfg['compiler_args'] = ['-std=c++14', '-fvisibility=hidden']
 cfg['linker_args'] = ['-fvisibility=hidden']
 cfg['include_dirs'] = ['sdsl-lite/include']
 cfg['libraries'] = ['sdsl', 'divsufsort', 'divsufsort64']
-cfg['dependencies'] = ['converters.hpp', 'pysequence.hpp', 'sizes.hpp']
+cfg['dependencies'] = ['converters.hpp', 'pysequence.hpp',
+                       'sizes.hpp', 'calc.hpp']
 %>
 */
 
-#include <algorithm>
 #include <cstdint>
 #include <fstream>
 #include <string>
@@ -20,6 +20,7 @@ cfg['dependencies'] = ['converters.hpp', 'pysequence.hpp', 'sizes.hpp']
 
 #include <pybind11/pybind11.h>
 
+#include "calc.hpp"
 #include "converters.hpp"
 #include "sizes.hpp"
 
@@ -28,87 +29,6 @@ namespace py = pybind11;
 
 
 using sdsl::int_vector;
-
-
-template <class Sequence>
-auto add_std_algo(py::class_<Sequence>& cls)
-{
-    cls.def(
-        "__iter__",
-        [](const Sequence &s) { return py::make_iterator(s.begin(), s.end()); },
-        py::keep_alive<0, 1>()
-    );
-    cls.def(
-        "__contains__",
-        [](const Sequence &self, typename Sequence::value_type element) {
-            return std::find(self.begin(), self.end(), element) != self.end();
-        },
-        py::call_guard<py::gil_scoped_release>()
-    );
-    cls.def(
-        "__getitem__",
-        [](const Sequence &self, size_t position) {
-            if (position >= self.size())
-            {
-                throw py::index_error(std::to_string(position));
-            }
-            return self[position];
-        }
-    );
-    cls.def(
-        "__getitem__",
-        [](const Sequence &self, int64_t position) {
-            auto abs_position = std::abs(position);
-            if (position >= 0)
-            {
-                throw std::exception();
-            }
-            if (abs_position > self.size())
-            {
-                throw py::index_error(std::to_string(position));
-            }
-            return self[self.size() - abs_position];
-        }
-    );
-    cls.def(
-        "max",
-        [](const Sequence &self) {
-            return *std::max_element(self.begin(), self.end());
-        },
-        py::call_guard<py::gil_scoped_release>()
-    );
-    cls.def(
-        "min",
-        [](const Sequence &self) {
-            return *std::min_element(self.begin(), self.end());
-        },
-        py::call_guard<py::gil_scoped_release>()
-    );
-    cls.def(
-        "minmax",
-        [](const Sequence &self) {
-            auto result = std::minmax_element(self.begin(), self.end());
-            return std::make_pair(*std::get<0>(result), *std::get<1>(result));
-        },
-        py::call_guard<py::gil_scoped_release>()
-    );
-    cls.def(
-        "sum",
-        [](const Sequence &self) {
-            return std::accumulate(self.begin(), self.end(),
-                                   uint64_t(0));
-        },
-        py::call_guard<py::gil_scoped_release>()
-    );
-    cls.def(
-        "is_sorted",
-        [](const Sequence &self) {
-            return std::is_sorted(self.begin(), self.end());
-        }
-    );
-
-    return cls;
-}
 
 
 template <class T>
