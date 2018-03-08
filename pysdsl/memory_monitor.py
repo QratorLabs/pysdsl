20bdd37bb5d9d6f098cbfb10ba86d84c0a99a3fe
Konstantin Podshumok <kpp.live@gmail.com>
2018-03-08 21:32:25 +0300

    full (?) memory monitor support

--- pysdsl/memory_monitor.py
+++ pysdsl/memory_monitor.py
@@ -1,13 +1,21 @@
-from pysdsl import _memory_monitor_start, _memory_monitor_stop
+from pysdsl import _memory_monitor_start, _memory_monitor_stop, \
+                   _memory_monitor_report_json, _memory_monitor_report_html
 
 
 class MemoryMonitor(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, out_html=None, out_json=None):
+        self.out_html = out_html
+        self.out_json = out_json
 
     def __enter__(self):
         _memory_monitor_start()
 
     def __exit__(self, exc_type, exc_value, traceback):
         _memory_monitor_stop()
+
+        if self.out_html is not None:
+            _memory_monitor_report_html(self.out_html)
+
+        if self.out_json is not None:
+            _memory_monitor_report_json(self.out_json)
