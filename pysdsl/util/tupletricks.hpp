1512216039b3ed69c85ec684377548a2a5583208
Dmitrii Kamaldinov <dimak24.05@mail.ru>
2019-03-23 15:22:21 +0300

    forward each in tuple

--- pysdsl/util/tupletricks.hpp
+++ pysdsl/util/tupletricks.hpp
@@ -27,6 +27,26 @@ constexpr
 decltype(auto) for_each(std::tuple<T...>& t, Function&& f) {
     return for_each_impl(t, f, std::index_sequence_for<T...>{}); }
 
+template<typename P, typename Function, std::size_t... Is>
+constexpr
+decltype(auto) forward_each_impl(P&& t, Function&& f, std::index_sequence<Is...>) {
+    return std::forward_as_tuple(f(std::get<Is>(t))...); }
+
+template<typename P, typename Function, std::size_t... Is>
+constexpr
+decltype(auto) forward_each_impl(P& t, Function&& f, std::index_sequence<Is...>) {
+    return std::forward_as_tuple(f(std::get<Is>(t))...); }
+
+template<typename... T, typename Function>
+constexpr
+decltype(auto) forward_each(const std::tuple<T...>& t, Function&& f) {
+    return forward_each_impl(t, f, std::index_sequence_for<T...>{}); }
+
+template<typename... T, typename Function>
+constexpr
+decltype(auto) forward_each(std::tuple<T...>& t, Function&& f) {
+    return forward_each_impl(t, f, std::index_sequence_for<T...>{}); }
+
 }  // namespace detail
 
 
@@ -40,3 +60,14 @@ template <typename... Ts, typename F>
 constexpr
 decltype(auto) for_each_in_tuple(std::tuple<Ts...> &t, F f) {
     return detail::for_each(t, f); }
+
+template <typename... Ts, typename F>
+constexpr
+decltype(auto) forward_each_in_tuple(const std::tuple<Ts...> &t, F f) {
+    return detail::forward_each(t, f); }
+
+
+template <typename... Ts, typename F>
+constexpr
+decltype(auto) forward_each_in_tuple(std::tuple<Ts...> &t, F f) {
+    return detail::forward_each(t, f); }
