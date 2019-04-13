f0b3e7cc7a8b3ad2226df41d1a249970989bbca4
Dmitrii Kamaldinov <dimak24.05@mail.ru>
2019-04-13 03:25:17 +0300

    rmq as params

--- pysdsl/types/rmq.hpp
+++ pysdsl/types/rmq.hpp
@@ -53,6 +53,9 @@ namespace RAC_names {
     const char INT_VECTOR_NAME[] = "IntVector";
 }
 
+template <typename t_rac, typename t_min_ic>
+using general_rmq_sparse_table = py::class_<sdsl::rmq_support_sparse_table<t_rac, t_min_ic::value>>;
+
 
 inline auto add_rmq_classes(py::module& m) {
     m.attr("rmq_sparse_table") = py::dict();
@@ -69,6 +72,16 @@ inline auto add_rmq_classes(py::module& m) {
 
     auto rmq_sparse_tables = for_each_in_tuple(rmq_support_sparse_table_params(), 
                                 add_rmq_sparse_table_functor(m, doc_rmq_sparse_table));
+
+    ////////////// as params //////////////////////
+    using rmq_support_sparse_table_as_params = std::tuple<
+        std::tuple<sdsl::int_vector<>, std::integral_constant<bool, true>>,
+        std::tuple<sdsl::int_vector<>, std::integral_constant<bool, false>>
+    >;
+
+    auto rmq_sparse_tables_as_params = for_each_in_tuple(rmq_support_sparse_table_as_params(), 
+                                make_general_sybset_functor<general_rmq_sparse_table>(rmq_sparse_tables));
+    ///////////////////////////////////////////////
     
     return std::tuple_cat(rmq_sparse_tables);
 }
