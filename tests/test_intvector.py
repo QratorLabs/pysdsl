5731b737e610e410d0a38a07ac06fa1128c95bf3
Ivan Ignatev <ignatev.is@phystech.edu>
2019-04-18 19:16:40 +0300

    added few more tests

--- tests/test_intvector.py
+++ tests/test_intvector.py
@@ -1,9 +1,8 @@
-import array
-import pysdsl
+from pysdsl import *
 
 
 def test_intvector():
-    v = pysdsl.IntVector(1024 * 1024 * 256)
+    v = IntVector(1024 * 1024 * 256)
     assert v.size_in_mega_bytes < 2049
     v.set_to_id()
     assert v.width == 64
@@ -11,6 +10,19 @@ def test_intvector():
     assert v.width == 28
     assert v.size_in_mega_bytes < 900
 
-    v = pysdsl.IntVector([3, 2, 1, 0, 2, 1, 3, 4, 1, 1, 1, 3, 2, 3])
-    v.bit_compress()
-    assert sorted(v)[2] == 1
+    for T in [IntVector, Int4Vector, Int8Vector, Int16Vector, Int24Vector,
+              Int32Vector, Int64Vector]:
+        v = IntVector([3, 2, 1, 0, 2, 1, 3, 4, 1, 1, 1, 3, 2, 3])
+        v.bit_compress()
+        assert sorted(v)[2] == 1
+
+    v_len = 10
+    bit_v = BitVector(v_len)
+    for i in range(v_len):
+        bit_v[i] = i % 2
+    bit_v.flip()
+    assert bit_v.width == 1
+    assert bit_v.max() == True
+    assert bit_v.min() == False
+    bit_v = BitVector([1, 0, 1])
+    assert bit_v.bit_size == 3
