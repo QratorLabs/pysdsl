336136c4a89c3b0be470e79f8f1046a4d3a29129
Ivan Ignatev <ignatev.is@phystech.edu>
2019-04-24 03:20:52 +0300

    fixed suffixarray test

--- tests/test_suffixarray.py
+++ tests/test_suffixarray.py
@@ -2,12 +2,24 @@ import pysdsl
 import pytest
 
 
-@pytest.mark.parametrize("Type", [# pysdsl.SuffixArraySadakaneInt,
-                                  # pysdsl.SuffixArrayWaveletTreeInt,
-                                  pysdsl.SuffixArrayWaveletTree,
+@pytest.mark.parametrize("Type", [pysdsl.SuffixArraySadakaneInt,
+                                  pysdsl.SuffixArrayWaveletTreeInt])
+def test_int_suffixarray(Type):
+    a = Type([3, 2, 1, 5, 2, 1, 3, 4, 1, 1, 1, 3, 2, 1])
+    assert a.count([3, 2, 1]) == 2
+    assert a.count([2, 1]) == 3
+    assert a.count([1, 2]) == 0
+    assert a.count([1, 1]) == 2
+    assert a.count([1]) == 6
+    assert a.count([]) == 16
+    assert a.text[5] == 2
+    assert a.sigma == 7
+
+
+@pytest.mark.parametrize("Type", [pysdsl.SuffixArrayWaveletTree,
                                   pysdsl.SuffixArraySadakane,
                                   pysdsl.SuffixArrayBitcompressed])
-def test_suffixarray(Type):
+def test_char_suffixarray(Type):
     a = Type("abracadabra")
     assert a.count("abr") == 2
     assert a.count("a") == 5
